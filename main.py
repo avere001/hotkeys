e6f603e0ed4b8b719c0e49b9df3c2448007614a5
Adam <5581459+avere001@users.noreply.github.com>
2022-10-29 16:27:55 -0700

    Use pynput types

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
+from typing import Union
+
 import pyautogui
 
 from pynput import keyboard
+from pynput.keyboard import KeyCode, Key
 
 
-def on_press(key):
-    try:
-        k = key.char  # single-char keys
-    except AttributeError:
-        k = key.name  # other keys
-
-    if k == 'page_up':
+def on_press(key: Union[Key, KeyCode]):
+    if key == Key.page_up:
         print("holding e..")
         pyautogui.keyUp("e")
         pyautogui.keyDown("e")
-    elif k == 'pause':
+    elif key == Key.pause:
         print("holding right click..")
-        pyautogui.mouseUp(button='right')
-        pyautogui.mouseDown(button='right')
+        pyautogui.mouseUp(button="right")
+        pyautogui.mouseDown(button="right")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
     listener.join()
