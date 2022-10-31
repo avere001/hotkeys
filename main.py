7256d46f4dc19e8c45c0b93fe6650e07c580fe80
Adam <5581459+avere001@users.noreply.github.com>
2022-10-31 13:20:15 -0700

    Add spam e on page down

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+from threading import Event, Thread
 from typing import Union
 
 import pyautogui
-
 from pynput import keyboard
 from pynput.keyboard import KeyCode, Key
+
+spam_e_event = Event()
+
+
+def spam_e_task():
+    while True:
+        if spam_e_event.is_set():
+            pyautogui.press("e")
 
 
 def on_press(key: Union[Key, KeyCode]):
@@ -11,6 +19,8 @@ def on_press(key: Union[Key, KeyCode]):
         print("holding e..")
         pyautogui.keyUp("e")
         pyautogui.keyDown("e")
+    elif key == Key.page_down:
+        spam_e_event.clear() if spam_e_event.is_set() else spam_e_event.set()
     elif key == Key.pause:
         print("holding right click..")
         pyautogui.mouseUp(button="right")
@@ -18,6 +28,11 @@ def on_press(key: Union[Key, KeyCode]):
 
 
 if __name__ == "__main__":
+    spam_e_thread = Thread(target=spam_e_task)
     listener = keyboard.Listener(on_press=on_press)
+
     listener.start()
+    spam_e_thread.start()
+
     listener.join()
+    spam_e_thread.join()
